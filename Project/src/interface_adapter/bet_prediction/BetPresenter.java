d97921acb19153cae6cdea8fb1efb30905863bc7
haoyuc0127@gmail.com <haoyuc0127@gmail.com>
2023-11-12 15:51:04 -0500

    Implemented controller, input boundary, output boundary, input data and output data first the first use case, started on the interactor and presenter.

--- Project/src/interface_adapter/bet_prediction/BetPresenter.java
+++ Project/src/interface_adapter/bet_prediction/BetPresenter.java
@@ -5,7 +5,7 @@ import use_case.bet_predictor.BetOutputData;
 
 public class BetPresenter implements BetOutputBoundary {
     @Override
-    public void prepareSuccessView(BetOutputData team) {
+    public void prepareSuccessView(BetOutputData teams) {
 
     }
 
