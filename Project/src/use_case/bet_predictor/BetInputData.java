6279bd65154151c5c943e7508626cf58ca19ffbb
haoyuc0127@gmail.com <haoyuc0127@gmail.com>
2023-11-12 15:50:18 -0500

    Implemented controller, input boundary, output boundary, input data and output data first the first use case, started on the interactor and presenter.

--- Project/src/use_case/bet_predictor/BetInputData.java
+++ Project/src/use_case/bet_predictor/BetInputData.java
@@ -2,15 +2,15 @@ package use_case.bet_predictor;
 
 public class BetInputData {
 
-    private final String team1Name;
-    private final String team2Name;
+    private final String team1;
+    private final String team2;
 
     public BetInputData(String team1, String team2) {
-        this.team1Name = team1;
-        this.team2Name = team2;
+        this.team1 = team1;
+        this.team2 = team2;
 
     }
-    String getTeam1Name(){return this.team1Name;}
-    String getTeam2Name(){return this.team2Name;}
+    String getTeam1Name(){return this.team1;}
+    String getTeam2Name(){return this.team2;}
 
 }
