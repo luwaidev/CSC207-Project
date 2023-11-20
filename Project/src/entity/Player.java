f4cc727d0d0f5e7e78f3c19b0efc39262347c0d6
mmaizzie <maissie.lin@mail.utoronto.ca>
2023-11-20 17:38:57 -0500

    added player class and created player data access object (still need method to get from user-input "name" to convert to playerId -> do later).

---
+++ Project/src/entity/Player.java
@@ -0,0 +1,35 @@
+package entity;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Player {
+    private final int playerId;
+
+    private final int numGamesPlayed;
+    private final ArrayList<Integer> pointsPerGame;
+    private final double avgPoints;
+    public int getId() {return playerId;}
+
+    public int getNumGamesPlayed() {return numGamesPlayed;}
+
+    public ArrayList<Integer> getPointsPerGame(){ return pointsPerGame;}
+
+    public double getAvgPoints(){return avgPoints;}
+
+    public Player(int playerId, ArrayList<Integer> playerPointsPerGame){
+        this.playerId = playerId;
+        this.pointsPerGame = playerPointsPerGame;
+        this.numGamesPlayed = pointsPerGame.size();
+        this.avgPoints = avg(playerPointsPerGame, numGamesPlayed);
+    }
+
+    public double avg(ArrayList<Integer> playerPointsPerGame, int numGamesPlayed){
+        int total = 0;
+        for (int point : playerPointsPerGame) {
+            total += point;
+        }
+        return (double) total/numGamesPlayed;
+    }
+
+}
