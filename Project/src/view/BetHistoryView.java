fcd3e5fab6d637a756d7872ce650bb67297ef4ef
Umar <syedu0324@gmail.com>
2023-11-26 13:45:40 -0500

    Added BetHistory Button to menu

---
+++ Project/src/view/BetHistoryView.java
@@ -0,0 +1,9 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+public class BetHistoryView extends JPanel{
+    public final String viewName = "Bet History";
+}
