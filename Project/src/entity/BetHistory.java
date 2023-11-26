663331cc0e72ecc45a0f89371ee7d0ed81b5b117
Umar <syedu0324@gmail.com>
2023-11-26 12:36:01 -0500

    Finished BetHistory Entity, added in the gethistory method

--- Project/src/entity/BetHistory.java
+++ Project/src/entity/BetHistory.java
@@ -1,4 +1,20 @@
 package entity;
 
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.NoSuchElementException;
+
 public class BetHistory {
+    private final Dictionary<CommonUser, ArrayList<String>> betHistory = new Hashtable<>();
+
+    public ArrayList<String> getHistory(CommonUser user) {
+        if (betHistory.get(user) != null) {
+            return betHistory.get(user);
+        }else {
+            throw new NoSuchElementException("No such user exists!");
+        }
+    };
+
+
 }
