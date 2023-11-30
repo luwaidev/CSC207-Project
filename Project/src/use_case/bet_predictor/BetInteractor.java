7ababe6d34e863737fa95ee6177760265eee2edf
Syed Umar <62903521+syedumar-03@users.noreply.github.com>
2023-11-30 09:35:48 -0500

    Betinteractor integration

--- Project/src/use_case/bet_predictor/BetInteractor.java
+++ Project/src/use_case/bet_predictor/BetInteractor.java
@@ -1,6 +1,8 @@
 package use_case.bet_predictor;
 import data_access.TeamDataAccessObject;
+import entity.BetHistory;
 import entity.Team;
+
 
 public class BetInteractor implements BetInputBoundary {
     // this class is for making predictions b/w two teams
@@ -18,19 +20,22 @@ public class BetInteractor implements BetInputBoundary {
     public void execute(BetInputData betInputData) {
         String team1 = betInputData.getTeam1Name();
         String team2 = betInputData.getTeam2Name();
+        String username = betInputData.getUsername();
         String error = "Error";
 
         Team fteam = TeamDataAccessObject.getTeamStats(TeamDataAccessObject.getTeamID(team1));
         Team steam = TeamDataAccessObject.getTeamStats(TeamDataAccessObject.getTeamID(team2));
-
         if (fteam.getAvg_points() > steam.getAvg_points()) {
             BetOutputData betWinner = new BetOutputData(team1, betInputData.panel);
+            String bet = team1 + " will beat " + team2;
+            BetHistory.setBetHistory(username, bet);
             betPresenter.prepareSuccessView(betWinner);
         }
         else if (fteam.getAvg_points() < steam.getAvg_points()) {
             BetOutputData betWinner = new BetOutputData(team2, betInputData.panel);
+            String bet = team2 + " will beat " + team1;
+            BetHistory.setBetHistory(username, bet);
             betPresenter.prepareSuccessView(betWinner);
-
         }
         else{
             BetOutputData betWinner = new BetOutputData(error, betInputData.panel);
