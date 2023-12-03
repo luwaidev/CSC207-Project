41b331a8ec45594dfd6accb12e63678b2f0f5b67
haoyuc0127@gmail.com <haoyuc0127@gmail.com>
2023-12-02 19:24:37 -0500

    fixed interactor for recommendation use case, fixed the button,

--- Project/src/use_case/bet_recommendation/RecommendationInteractor.java
+++ Project/src/use_case/bet_recommendation/RecommendationInteractor.java
@@ -4,7 +4,7 @@ import data_access.PlayerDataAccessObject;
 import data_access.TeamDataAccessObject;
 import entity.Player;
 import entity.Team;
-import use_case.bet_predictor.BetOutputData;
+
 
 import java.util.ArrayList;
 
@@ -19,17 +19,21 @@ public class RecommendationInteractor implements RecommendationInputBoundary{
     }
     @Override
     public void execute(RecommendationInputData recommendationInputData) {
-        String first_name = recommendationInputData.getFirst_name();
-        String last_name = recommendationInputData.getLast_name();
+        String name = recommendationInputData.getName();
+
+        String[] names = name.split(" ");
+        String first_name = names[0];
+        String last_name = names[1];
         String error = "Error";
+
 
         Player fav_player = PlayerDataAccessObject.getPlayerStats(PlayerDataAccessObject.getPlayerID(first_name, last_name));
         ArrayList <Integer> points = fav_player.getPointsPerGame();
 
         Double over_avg = Math.round(fav_player.avg(points,points.size())) + 0.5;
         Double under_avg = Math.round(fav_player.avg(points,points.size())) - 0.5;
-        Double over_avg1 = Math.round(fav_player.avg(points,points.size())) + 1.5;
-        Double under_avg1 = Math.round(fav_player.avg(points,points.size())) - 1.5;
+        Double over_avg1 = Math.round(fav_player.avg(points,points.size())) + 2.5;
+        Double under_avg1 = Math.round(fav_player.avg(points,points.size())) - 2.5;
 
             String safe_bet = "Safe Bet : \n";
             String over = first_name + " " + last_name + " scoring over " + over_avg1 +" pts"  + "\n";
